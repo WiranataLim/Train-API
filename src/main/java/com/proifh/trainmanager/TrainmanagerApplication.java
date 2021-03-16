1f0091e40b34e9889bf13fc9e7e2c6dbcff849b2
Jiang Han <jianghan99@gmail.com>
2021-03-16 15:13:14 +0700

    initial commit
    Spring project on Netbeans with Maven

---
+++ src/main/java/com/proifh/trainmanager/TrainmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.proifh.trainmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proifh/trainmanager/TrainmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proifh.trainmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
