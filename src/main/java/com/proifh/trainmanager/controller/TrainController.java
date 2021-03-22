6a852e8a1bd0dea12e5b056d76bddddf9930a52e
Xilth <el.jason156@gmail.com>
2021-03-22 10:28:18 +0700

    Unused Line Cleaning + getById API

--- src/main/java/com/proifh/trainmanager/controller/TrainController.java
+++ src/main/java/com/proifh/trainmanager/controller/TrainController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.proifh.trainmanager.model.Train;
 import com.proifh.trainmanager.repository.TrainRepository;
 import java.util.ArrayList;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,6 +37,17 @@ public class TrainController {
             return new ResponseEntity<>(trains, HttpStatus.OK);
         } catch (Exception e){
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @GetMapping("/trains/{id}")
+    public ResponseEntity<Train> getTrainById(@PathVariable("id") long id){
+        Optional<Train> trainData = trainRepository.findById(id);
+        
+        if(trainData.isPresent()){
+            return new ResponseEntity<>(trainData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
