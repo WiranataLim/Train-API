54ac55cdd91d5b7f8fa1f699a3aa3527d8fa58c9
Xilth <el.jason156@gmail.com>
2021-03-22 12:37:35 +0700

    get amenities API implementation + fixes

--- src/main/java/com/proifh/trainmanager/repository/TrainRepository.java
+++ src/main/java/com/proifh/trainmanager/repository/TrainRepository.java
@@ -1,8 +1,9 @@
 package com.proifh.trainmanager.repository;
 
 import com.proifh.trainmanager.model.Train;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TrainRepository extends JpaRepository<Train, Long> {
-    
+    List<Train> findByAmenitiesContaining(String keyword);
 }
