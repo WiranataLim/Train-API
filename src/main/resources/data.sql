23c3b9bb6e876f1b346eeb9a4f7212736b5cbb72
WiranataLim <6181801038@student.unpar.ac.id>
2021-03-22 12:42:04 +0800

    Perbaikan data path data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,74 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  LIM
+ * Created: Mar 21, 2021
+ */
+
+INSERT INTO train (id, name, description, distance_between_stop, max_speed, sharing_tracks, grade_crossing, train_frequency, amenities)
+VALUES(
+    1,
+    'Light rail',
+    'Light rail, which might be also known as trolley and streetcars',
+    'a few blocks to 1 or 2 miles',
+    '55-65 mph',
+    true,
+    false,
+    '3-30 minutes',
+    'n/a'
+);
+
+INSERT INTO train (id, name, description, distance_between_stop, max_speed, sharing_tracks, grade_crossing, train_frequency, amenities)
+VALUES(
+    2,
+    'Rapid transit',
+    'Rapid transit, which is also known as metro',
+    '1/2 mile to 2 or 3 miles',
+    '65-70 mph',
+    false,
+    false,
+    '3-20 minutes',
+    'Large space for standees'
+);
+
+INSERT INTO train (id, name, description, distance_between_stop, max_speed, sharing_tracks, grade_crossing, train_frequency, amenities)
+VALUES(
+    3,
+    'Commuter rail',
+    'Commuter trains generally mean trains connecting suburban areas',
+    '1 to 2 miles',
+    '79 mph',
+    true,
+    true,
+    '15 minutes to hourly (some operate only during weekday peak hours)',
+    'Restroom'
+);
+
+INSERT INTO train (id, name, description, distance_between_stop, max_speed, sharing_tracks, grade_crossing, train_frequency, amenities)
+VALUES(
+    4,
+    'Inter-city rail',
+    'Inter-city trains generally mean trains traveling long distances connecting metropolitan areas.',
+    '2 to 10 miles',
+    '79 mph',
+    true,
+    true,
+    'Hourly to once a day, or even three trips a week.',
+    'Restroom, business class, sleeper (longer distance), cafe car, dining car'
+);
+
+INSERT INTO train (id, name, description, distance_between_stop, max_speed, sharing_tracks, grade_crossing, train_frequency, amenities)
+VALUES(
+    5,
+    'High speed rail',
+    'High speed trains are generally defined as trains that can operate 125mph or faster.',
+    'at least 10 miles',
+    'at least 90 mph',
+    false,
+    false,
+    '15 minutes to hourly',
+    'Restroom, business class, cafe car'
+);
