7881616c65dd5b0deb7fc3087e06603c7e047281
Jiang Han <jianghan99@gmail.com>
2021-04-12 19:46:17 +0700

    Add Custom Exception Handler on Missing Key when posting new Train

--- src/main/java/com/proifh/trainmanager/controller/ExceptionResolver.java
+++ src/main/java/com/proifh/trainmanager/controller/ExceptionResolver.java
@@ -6,6 +6,7 @@
 package com.proifh.trainmanager.controller;
 import java.util.HashMap;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
@@ -20,9 +21,16 @@ public class ExceptionResolver {
     @ExceptionHandler({NoHandlerFoundException.class, MethodArgumentTypeMismatchException.class, MissingServletRequestParameterException.class})
     @ResponseStatus(value= HttpStatus.METHOD_NOT_ALLOWED)
     public HashMap<String, String> handleNoHandlerFound(Exception e, WebRequest request) {
-        System.out.println("Resolving...");
         HashMap<String, String> response = new HashMap<>();
         response.put("message", "invalid endpoint");
+        return response;
+    }
+    
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(value= HttpStatus.BAD_REQUEST)
+    public HashMap<String, String> BadPostRequest(Exception e, WebRequest request) {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", "failed validation");
         return response;
     }
 }
